e57c62ef29d19fe1904179edacfd2ebb489ad329
divyam-1612 <divymjainjain@gmail.com>
2024-05-28 01:12:01 +0530

    commit

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
     cors({
-        origin:"http://localhost:3000",
+        origin:"https://study-notion-flnjjpzrq-divyam-jains-projects.vercel.app/",
         credentials: true,
     })
 )
