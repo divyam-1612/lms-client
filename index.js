0c348ba27e8ebd0cda6d6163fac2bf6ff1bfb15c
divyam-1612 <divymjainjain@gmail.com>
2024-09-05 17:35:50 +0530

    first committt

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
     cors({
-        origin:"https://server-ochre-kappa.vercel.app/",
+        origin:"http://localhost:3000",
         credentials: true,
     })
 )
--- utils/imageUploader.js
+++ utils/imageUploader.js
@@ -14,4 +14,4 @@ exports.uploadImageToCloudinary = async(file, folder, height, quality) => {
     options.resource_type = "auto";
 
     return await cloudinary.uploader.upload(file.tempFilePath , options)
-}
+}   
